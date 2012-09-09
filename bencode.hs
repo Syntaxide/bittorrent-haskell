94824daedf2d3ae1de6eab3ea516f29e89e14887
syntaxide <syntaxide@nethawk.(none)>
2012-09-08 19:43:26 -0500

    swapped string with ByteString, fixed related bugs. No longer crashes upon parsing .torrent

--- bencode.hs
+++ bencode.hs
@@ -9,37 +9,44 @@
 --containers:
 --  list	l[BENC]e
 --  dict	d[BENCSTR BENCVAL]e
+import Prelude hiding ((!!))
 import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import qualified Data.ByteString.Char8 as BS
 
-data BEVal 	=  BEInt Int | BEStr String | BEDict (Map.Map String BEVal)  | BEList [BEVal]
+data BEVal 	=  BEInt Int | BEStr BS.ByteString | BEDict (Map.Map BS.ByteString BEVal)  | BEList [BEVal]
 		deriving (Show)
 			    
-{-slurpBEValue :: String -> BEValue
-slurpBEValue (x:xs) = BEString (x:' ':xs)-}
+(!!) = BS.index	--convenient alias
 
-subString :: String -> Int -> Int -> String
-subString hay start len = take len $ drop start hay
+subString :: BS.ByteString -> Int -> Int -> BS.ByteString
+subString hay start len = BS.take len $ BS.drop start hay
 
+toBSInt :: BS.ByteString -> Int
+toBSInt s = first$Maybe.fromJust$BS.readInt s
+
+toBSList s = BS.groupBy (\a b -> True) s
 first (a,_) = a
 second (_,b) = b
 apply f a = f a
 -- i1234e5hello
 -- be functions should return a tuple. (VALUE, endposition)
-slurpBEInt :: String -> Int -> (BEVal, Int)
-slurpBEInt (_:xs) pos = 
-	let str = takeWhile (/= 'e') (drop pos xs) 	--drop first char(i), read till e
-	in (BEInt$read str, pos + length str + 2)
+slurpBEInt :: BS.ByteString -> Int -> (BEVal, Int)
+slurpBEInt x pos = 
+	let str = BS.takeWhile (/= 'e') (BS.drop pos xs) 	--drop first char(i), read till e
+	in (BEInt$toBSInt str, pos + BS.length str + 2)
+	where xs = BS.drop 1 x
 	
-slurpBEStr :: String -> Int -> (BEVal, Int)
+slurpBEStr :: BS.ByteString -> Int -> (BEVal, Int)
 slurpBEStr str pos = 
 	let 
-	s = drop pos str
-	a = takeWhile (/= ':') s
-	b = 1 + length a
-	c = subString s b (read a :: Int)
-	in (BEStr$c, pos + b+read a) 
+	s = BS.drop pos str
+	a = BS.takeWhile (/= (':' )) s
+	b = 1 + BS.length a
+	c = subString s b (toBSInt a)
+	in (BEStr$c, pos + b+toBSInt a) 
 
-slurpBEList :: String ->  Int -> ([BEVal], Int) --expects pos to be beyond the opening l
+slurpBEList :: BS.ByteString ->  Int -> ([BEVal], Int) --expects pos to be beyond the opening l
 slurpBEList str pos =
 	let (barr,end) = slurpBEVal str pos 
 	    (next,end2)= if str !! end == 'e' then ([],end+1) else slurpBEList str end
@@ -51,7 +58,7 @@ slurpBEList str pos =
 packBEList :: ([BEVal], Int) -> (BEVal, Int)
 packBEList (val, end) = (BEList val, end)
 
-slurpBEVal :: String -> Int -> (BEVal, Int)
+slurpBEVal :: BS.ByteString -> Int -> (BEVal, Int)
 slurpBEVal str pos  
 	| x `elem` ['0'..'9'] = (slurpBEStr str pos)
 	| x == 'i' = (slurpBEInt str pos)
@@ -61,5 +68,5 @@ slurpBEVal str pos
 
 
 main = do
-	s <- readFile "torrent.torrent"
+	s <- BS.readFile "data/ubuntu.torrent"
 	print$slurpBEVal s 0
